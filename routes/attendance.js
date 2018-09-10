1234cf08f5df5fc6c30695c395c7e2a2b7fa78fb
Kashif <mailmethis@protonmail.com>
2018-09-11 00:23:58 +0530

    Attendance.

--- routes/attendance.js
+++ routes/attendance.js
@@ -1,6 +1,14 @@
 const express = require('express')
-const mysql = require('../config/mysql')
+const passport = require('passport')
 
+const mysql = require('../config/mysql')
 const router = express.Router()
 
+router.post('/add', passport.authenticate('jwt', { session: false }), (req, res) => {
 
+  const { user } = req
+  const { sid, timeid, _date, present, pending } = req.body.data
+
+  
+
+})
