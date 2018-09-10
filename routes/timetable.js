18e1b3f735d229ead2ada78ea23779120164b3e8
Kashif <mailmethis@protonmail.com>
2018-09-10 16:30:12 +0530

    fixed a bug in timetable/add route.

--- routes/timetable.js
+++ routes/timetable.js
@@ -13,18 +13,17 @@ router.post('/add', passport.authenticate('jwt', { session: false }), (req, res)
   const { day, classes } = data
   const length = Object.keys(classes).length
   var count = 0
-  var timeid
 
   Object.keys(classes).map(key => {
 
     const data = classes[key]
     const { subject, timeFrom, timeTo } = data
-
     Promise
       .all([getTimeID(timeFrom, timeTo), getSubjectID(subject)])
       .then(responses => {
         const [ timeid, sid ] = responses
 
+        // check if the record exists
         const checkQuery = `select uid from timetable where uid = '${user.uid}' and day = '${day}' and sid = '${sid}' and timeid = '${timeid}'`
         mysql.query(
           checkQuery,
@@ -32,22 +31,23 @@ router.post('/add', passport.authenticate('jwt', { session: false }), (req, res)
             if(err)
               return console.log(err)
 
-            if(result.length !== 0)
+            if(result.length !== 0 && count === length-1)
               return res.json({ success: true })
-              
-            const insertQuery = `insert into timetable values('${user.uid}', '${day}', '${sid}', '${timeid}')`
-            mysql.query(
-              insertQuery,
-              (err, result, fields) => {
-                if(err)
-                  return console.log(err)
-  
-                if(count === length-1)
-                  return res.json(result)
-  
-                count++
-              }
-            )
+            // insert into timetable if it doesn't exist
+            else if(result.length === 0) {
+              const insertQuery = `insert into timetable values('${user.uid}', '${day}', '${sid}', '${timeid}')`
+              mysql.query(
+                insertQuery,
+                (err, result, fields) => {
+                  if(err)
+                    return console.log(err)
+                  
+                  if(count === length-1)
+                    return res.json(result)
+                }
+              )
+            }
+            count++
           }
         )
       })
